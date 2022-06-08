37dee2154d92e2d5067748c0cd42f54538e89455
Ai-Sasit <sasit2555@gmail.com>
2022-06-08 10:01:30 +0700

    update air-ticket

--- src/app/air-ticket/air-ticket.page.ts
+++ src/app/air-ticket/air-ticket.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { format, parseISO, getDate, getMonth, getYear } from 'date-fns';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-air-ticket',
@@ -7,16 +8,17 @@ import { format, parseISO, getDate, getMonth, getYear } from 'date-fns';
   styleUrls: ['./air-ticket.page.scss'],
 })
 export class AirTicketPage implements OnInit {
-
-  public segment: string = "one";
+  public segment: string = 'one';
   public date0: any;
   public date1: any;
   public date2: any;
 
-  constructor() {}
-  ngOnInit(): void {
-      
-  }
+  public people0: number = 0;
+  public people1: number = 0;
+  public people2: number = 0;
+
+  constructor(private alertController: AlertController) {}
+  ngOnInit(): void {}
 
   segmentChanged(ev: any) {
     this.segment = ev.detail.value;
@@ -32,4 +34,90 @@ export class AirTicketPage implements OnInit {
     this.date2 = format(parseISO(value), 'dd/MM/yyyy');
   }
 
+  async p0Prompt(event) {
+    if (event.target.checked) {
+      this.people0 = 0;
+    } else {
+      const alert = await this.alertController.create({
+        cssClass: 'my-custom-class',
+        header: 'กรอกจำนวนคน',
+        mode: 'ios',
+        inputs: [
+          {
+            name: 'people',
+            type: 'number',
+            placeholder: 'จำนวนคน',
+          },
+        ],
+        buttons: [
+          {
+            text: 'ยืนยัน',
+            handler: (value) => {
+              this.people0 = value.people;
+            },
+          },
+        ],
+      });
+
+      await alert.present();
+    }
+  }
+
+  async p1Prompt(event) {
+    if (event.target.checked) {
+      this.people1 = 0;
+    } else {
+      const alert = await this.alertController.create({
+        cssClass: 'my-custom-class',
+        header: 'กรอกจำนวนคน',
+        mode: 'ios',
+        inputs: [
+          {
+            name: 'people',
+            type: 'number',
+            placeholder: 'จำนวนคน',
+          },
+        ],
+        buttons: [
+          {
+            text: 'ยืนยัน',
+            handler: (value) => {
+              this.people1 = value.people;
+            },
+          },
+        ],
+      });
+
+      await alert.present();
+    }
+  }
+
+  async p2Prompt(event) {
+    if (event.target.checked) {
+      this.people2 = 0;
+    }else{
+      const alert = await this.alertController.create({
+        cssClass: 'my-custom-class',
+        header: 'กรอกจำนวนคน',
+        mode: 'ios',
+        inputs: [
+          {
+            name: 'people',
+            type: 'number',
+            placeholder: 'จำนวนคน',
+          },
+        ],
+        buttons: [
+          {
+            text: 'ยืนยัน',
+            handler: (value) => {
+              this.people2 = value.people;
+            },
+          },
+        ],
+      });
+  
+      await alert.present();
+    }
+  }
 }
